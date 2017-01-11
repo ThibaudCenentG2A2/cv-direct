4c575c547e2ed9188c734c24442d126efb4b8db9
GeeksdelaCave <geeksdelacave@laposte.net>
2017-01-11 11:56:44 +0100

    Header & footer (à modifier)

---
+++ src/vue/footer.php
@@ -0,0 +1,61 @@
+
+<!-- footer start (Add "dark" class to #footer in order to enable dark footer) -->
+<!-- ================ -->
+<footer id="footer" class="clearfix ">
+
+    <!-- .subfooter start -->
+    <!-- ================ -->
+    <div class="subfooter">
+        <div class="container">
+            <div class="subfooter-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">© 2017 <a href="http://dromcorp.eu">DromCorp</a>. Tous droits réservés.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- .subfooter end -->
+
+</footer>
+<!-- footer end -->
+
+</div>
+<!-- page-wrapper end -->
+
+<!-- JavaScript files placed at the end of the document so the pages load faster -->
+<!-- ================================================== -->
+<!-- Jquery and Bootstap core js files -->
+<script type="text/javascript" src="plugins/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Modernizr javascript -->
+<script type="text/javascript" src="plugins/modernizr.js"></script>
+<!-- jQuery Revolution Slider  -->
+<script type="text/javascript" src="plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<!-- Isotope javascript -->
+<script type="text/javascript" src="plugins/isotope/isotope.pkgd.min.js"></script>
+<!-- Magnific Popup javascript -->
+<script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<!-- Appear javascript -->
+<script type="text/javascript" src="plugins/waypoints/jquery.waypoints.min.js"></script>
+<!-- Count To javascript -->
+<script type="text/javascript" src="plugins/jquery.countTo.js"></script>
+<!-- Parallax javascript -->
+<script src="plugins/jquery.parallax-1.1.3.js"></script>
+<!-- Contact form -->
+<script src="plugins/jquery.validate.js"></script>
+<!-- Owl carousel javascript -->
+<script type="text/javascript" src="plugins/owl-carousel/owl.carousel.js"></script>
+<!-- SmoothScroll javascript -->
+<script type="text/javascript" src="plugins/jquery.browser.js"></script>
+<script type="text/javascript" src="plugins/SmoothScroll.js"></script>
+<!-- Initialization of Plugins -->
+<script type="text/javascript" src="js/template.js"></script>
+<!-- Custom Scripts -->
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
+
