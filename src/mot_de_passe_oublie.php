bf4ba12ff010762246ebbb74edfcc5e53e556a25
Romain COLONNA D'ISTRIA <romain.colonna.distria@etu.univ-amu.fr>
2017-01-13 01:01:57 +0100

    Modif systeme oblie mdp

--- src/mot_de_passe_oublie.php
+++ src/mot_de_passe_oublie.php
@@ -8,7 +8,7 @@ if (isset($_POST['mail']))
     if (Recruteur::est_presente($_POST['mail']))
     {
         Courriel::envoyer_courriel_mot_de_passe_oublie($_POST['mail']);
-        echo 'Email envoyé avec succès !';
+        echo '<div class="alert alert-icon alert-success" role="alert"></div>';
     }
     else
         echo 'Email non reconnu. Veuillez un email associé a un compte.';
