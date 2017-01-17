4c1b9c53393563d1f80fe4983eac532f59180d0f
Thierryfe <fernandez27.tf@gmail.com>
2017-01-17 22:51:53 +0100

    ajout envoi de mail

--- src/inscription_recruteur.php
+++ src/inscription_recruteur.php
@@ -7,11 +7,15 @@
  */
 require_once 'header.php';
 require_once 'modele/Recruteur.php';
+require_once 'modele/Courriel.php';
 
 if (isset($_POST['pseudo_inscription']) &&
     isset($_POST['prenom_inscription']) && $_POST['nom_inscription'] && isset($_POST['mail_inscription']) && isset($_POST['mot_de_passe']))
+{
 
-    Recruteur::inscrire_utilisateur($_POST['pseudo_inscription'],$_POST['prenom_inscription'],$_POST['nom_inscription'],$_POST['mail_inscription'],$_POST['mot_de_passe']);
+    Recruteur::inscrire_utilisateur($_POST['pseudo_inscription'], $_POST['prenom_inscription'], $_POST['nom_inscription'], $_POST['mail_inscription'], $_POST['mot_de_passe']);
+    Courriel::envoyer_courriel_validation_par_mail($_POST['mail_inscription'],$_POST['pseudo_inscription'],"");
+}
 
 require_once 'vue/page_inscription.php';
 ?>
