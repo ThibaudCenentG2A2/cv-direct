02d0a0f67ecfdaa35370530a2b9be289e9d245d9
GeeksdelaCave <geeksdelacave@laposte.net>
2017-01-21 14:59:40 +0100

    Premier push de Competences

--- src/modele/Competences.php
+++ src/modele/Competences.php
@@ -1,12 +1,28 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: pyrrha
- * Date: 20/01/2017
- * Time: 18:09
- */
+require_once 'modele/BD.php';
+
 class Competences
 {
 
+    public static function get_competences() {
+
+        return BD::getInstance()->query('SELECT ID_COMPETENCE_CATEGORIE, NOM_COMPETENCE_CATEGORIE
+                                          FROM COMPETENCE_CATEGORIE
+                                          ORDER BY NOM_COMPETENCE_CATEGORIE');
+
+    }
+
+    /**
+     * Recherche si la catégorie donnée en paramètre existe bien dans la base de données
+     * @param $categorie
+     * @return bool
+     */
+    public static function categorie_existe($categorie) {
+
+        $req = BD::getInstance()->prepare('SELECT COUNT(*) AS TOTAL FROM COMPETENCE_CATEGORIE WHERE NOM_COMPETENCE_CATEGORIE = :nom_categorie');
+        $req->execute(array('nom_categorie' => $categorie));
+        $data = $req->fetch();
+        return $data['TOTAL'];
+    }
 }
