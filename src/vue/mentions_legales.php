9be8d3049db703ba29b1ba8e6c06e1a30bc9c66f
GeeksdelaCave <geeksdelacave@laposte.net>
2017-01-22 16:59:35 +0100

    Modification de la mise en page des CGU, liaison au template, ajout de texte

--- src/vue/mentions_legales.php
+++ src/vue/mentions_legales.php
@@ -1,37 +1,62 @@
 <?php
     require_once 'vue/header.php';
 ?>
-<section class="main-container" xmlns="http://www.w3.org/1999/html">
+            <section class="main-container">
 				<div class="container">
 					<div class="row">
-						<div class="main col-md-8">
-							<h1 class="page-title">Mentions Légales </h1> <br/>
-                            <p> En vigeur au 22/01/2017 </p>
-							<div class="separator-2"></div>
-                            <p> Conformément aux dispositions des articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la </p>
-                            <p> confiance dans l'économie numérique, dite L.C.E.N., il est porté à la connaissance des Utilisateurs </p>
-                            <p> du site <strong> http://cv-direct.alwaysdata.net/ </strong> les présentes mentions légales. </p>
-                            <p> La connexion et la navigation sur le site (http://cv-direct.alwaysdata.net/) par l'Utilisateur implique</p>
-                            <p> acceptation intégrale et sans réserve des présentes mentions légales. </p> <br/> <br/>
-                            <h1 class="page-title"> Article 1 : L'éditeur </h1> <br/>
-                            <p> L'édition et la direction de la publication du site <strong> http://cv-direct.alwaysdata.net/ </strong> est assurée par </p>
-                            <p> <strong> Equipe 3TN,</strong> domiciliée <strong> 413 avenue Gaston Berger, </strong> dont le numéro de téléphone est </p>
-                            <p> <strong> 0617977549, </strong> et l'adresse e-mail <strong> contact@cv-direct.fr </strong> . </p> <br/> <br/>
-                            <h1 class="page-title"> Article 2 : L'Hébergeur </h1> <br/>
-                            <p> L'hébergeur du site <strong> http://cv-direct.alwaysdata.net/ </strong> est la Société ALWAYSDATA, dont le siège</p>
-                            <p> social est situé au <strong> 62 rue Tiquettone-75002 Paris, </strong> avec le numéro de téléphone : <strong> +33.1 84 16 23</strong></p>
-                            <p> <strong> 40.</strong></p> <br/> <br/>
-                            <h1 class="page-title"> Article 3 : Accés Au Site </h1> <br/>
-                            <p> Le site est accessible par tout endroit, 7j/7j, 24h/24 sauf cas de force majeure, interruption</p>
-                            <p> programmée ou non et pouvant découler d'une nécessité de maintenance.</p>
-                            <p> En cas de modification, interruption ou suspension des services, le site<strong> http://cv-</strong></p>
-                            <p> <strong> direct.alwaysdata.net/</strong> ne saurait être tenu responsable.</p> <br/> <br/>
-                            <h1 class="page-title"> Article 4 : Collecte Des Données </h1> <br/>
-                            <p> Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans </p>
-                            <p> la mesure où il ne collecte aucune donnée concernant les utilisateurs. </p> <br/> <br/>
-                            <h1 class="page-title"> Article 5 : Cookies </h1> <br/>
-                            <p> Un cookie est un élément qui ne permet pas d'identifier l'Utilisateur mais sert à enregistrer des </p>
-                            <p> informations relatives à la naviguation de celui-ci</p>
+						<div class="main col-md-12">
+                            <div class="space-bottom">
+                                <h1 class="page-title">Mentions Légales </h1>
+                                <p> En vigeur au 22/01/2017 </p>
+                                <div class="separator-2"></div>
+                            </div>
+
+                            <div class="space-bottom">
+                                <p>Est désigné par le Site l'ensemble des pages web du site Internet CV-Direct, en ligne à l'adresse publique http://cv-direct.alwaysdata.net/.</p>
+                                <p>Est désigné ci-après comme Utilisateur toute personne se connectant au Site, en ayant les accès nécessaires et requis, dans le cadre de l'exploitation prévue lors de la conception du site Internet et lors de sa mise en ligne.</p>
+                                <p>Conformément aux dispositions :
+                                    <ul>
+                                        <li><strong>Du code de la consommation</strong> : articles L131-1 à L131-4</li>
+                                        <li><strong>Du code de la consommation</strong> : article R111-1</li>
+                                        <li><strong>De la confiance dans l'économie numérique</strong> : article 19 de la loi n°2004-575 du 21 juin 2004</li>
+                                        <li><strong>De la confiance dans l'économie numérique</strong> : loi n°2004-575 du 21 juin 2004, article 6</li>
+                                    </ul>
+                                    il est porté à la connaissance des Utilisateurs du Site les présentes mentions légales. </p>
+                                <p> La connexion et la navigation sur le site (http://cv-direct.alwaysdata.net/) par l'Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.</p>
+                            </div>
+
+                            <div class="space-bottom">
+                                <h2> Article 1 : L'éditeur </h2>
+                                <p> L'édition et la direction de la publication du site <strong> http://cv-direct.alwaysdata.net/ </strong> est assurée par </p>
+                                <p> <strong> Equipe 3TN,</strong> domiciliée <strong> 413 avenue Gaston Berger, </strong> dont le numéro de téléphone est </p>
+                                <p> <strong> 0617977549, </strong> et l'adresse e-mail <strong> contact@cv-direct.fr </strong> . </p>
+                            </div>
+
+                            <div class="space-bottom">
+                                <h2> Article 2 : L'Hébergeur </h2>
+                                <p> L'hébergeur du site <strong> http://cv-direct.alwaysdata.net/ </strong> est la Société ALWAYSDATA, dont le siège</p>
+                                <p> social est situé au <strong> 62 rue Tiquettone-75002 Paris, </strong> avec le numéro de téléphone : <strong> +33.1 84 16 23 40.</strong></p>
+                            </div>
+
+                            <div class="space-bottom">
+                                <h2> Article 3 : Accés Au Site </h2>
+                                <p> Le site est accessible par tout endroit, 7j/7j, 24h/24 sauf cas de force majeure, interruption</p>
+                                <p> programmée ou non et pouvant découler d'une nécessité de maintenance.</p>
+                                <p> En cas de modification, interruption ou suspension des services, le site<strong> http://cv-</strong></p>
+                                <p> <strong> direct.alwaysdata.net/</strong> ne saurait être tenu responsable.</p>
+                            </div>
+
+                            <div class="space-bottom">
+                                <h2> Article 4 : Collecte Des Données </h2>
+                                <p> Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans </p>
+                                <p> la mesure où il ne collecte aucune donnée concernant les utilisateurs. </p>
+                            </div>
+
+                            <div class="space-bottom">
+                                <h2> Article 5 : Cookies </h2>
+                                <p> Un cookie est un élément qui ne permet pas d'identifier l'Utilisateur mais sert à enregistrer des </p>
+                                <p> informations relatives à la naviguation de celui-ci</p>
+                            </div>
                         </div>
 					</div>
 				</div>
