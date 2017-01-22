f08607bc6089f9b9ee50a9663c583b2e0d3f0929
GeeksdelaCave <geeksdelacave@laposte.net>
2017-01-22 23:59:17 +0100

    Page de garde

--- src/vue/presentation.php
+++ src/vue/presentation.php
@@ -8,6 +8,24 @@
 
 require_once 'vue/header.php';
 
+?>
 
+    <section class="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="main col-md-12">
+                    <div class="space-bottom">
+                        <h1 class="page-title">CV-Direct</h1>
+                        <div class="separator-2"></div>
+                        <p>Bienvenue sur l'intranet de CV-Direct, votre agence pour l'emploi.</p>
+                        <p>Merci de vous connecter.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<?php
 
 require_once 'vue/footer.php';
